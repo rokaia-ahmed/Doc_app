af13ce5c632815b6aa48ecde73106eb53e160bfa
rokia <rokiaahmed42@gmail.com>
2024-12-22 18:12:13 +0200

    login api

---
+++ lib/core/networking/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstant{
+  static const String apiBaseUrl= 'https://vcare.integration25.com/api/';
+  static const String login = 'auth/login' ;
+  static const String register = 'auth/register' ;
+}
