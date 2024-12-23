434156eae4f9943a33cbaac48d2448df2f656c37
rokia <rokiaahmed42@gmail.com>
2024-12-23 17:54:07 +0200

    login api

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+import '../../features/login/data/models/login_request_body.dart';
+import '../../features/login/data/models/login_response.dart';
+import 'api_constants.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstant.apiBaseUrl)
+abstract class ApiService{
+  factory ApiService(Dio dio,{String baseUrl}) = _ApiService;
+
+  @POST(ApiConstant.login)
+  Future<LoginResponse> login(
+      @Body() LoginRequestBody loginRequestBody,
+      );
+
+  /*@POST(ApiConstant.signUp)
+  Future<SignupResponse> signup(
+      @Body() SignupRequestBody signupRequestBody,
+      );*/
+}
